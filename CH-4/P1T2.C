ae8c92c6d19ca06b542b98ca172bcd77920ae79e
Tanksujal <144035373+Tanksujal@users.noreply.github.com>
2023-09-12 19:31:00 +0530

    Update P1T2.C

--- CH-4/P1T2.C
+++ CH-4/P1T2.C
@@ -3,10 +3,15 @@ main(){
 	int salary,HRA,DA,TA;
 	printf("enter your salary = ");
 	scanf("%d",&salary);
-	salary = 1000;
-	HRA = salary*10/100;
-	DA = salary*5/100;
-	TA = salary*8/100;
+	printf("enter your HRA = ");
+	scanf("%d",&HRA);
+	printf("enter your DA = ");
+	scanf("%d",&DA);
+	printf("enter your TA = ");
+	scanf("%d",&TA);
+	HRA = salary*HRA/100;
+	DA = salary*DA/100;
+	TA = salary*TA/100;
 	salary = salary+HRA+DA+TA;
 	printf("your gross salary is = %d",salary);
 }
